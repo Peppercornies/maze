1719204ba8d6d5becd15e94e2ea374e8bd54293a
J. Park <jp711@pcmathsg14.maths.pwf.cam.ac.uk>
2019-11-22 17:06:03 +0000

    Commit

--- goodies.py
+++ goodies.py
@@ -8,6 +8,7 @@ import random
 
 from maze import Goody, Baddy, Position, UP, DOWN, LEFT, RIGHT, STAY, STEP, PING
 
+
 class StaticGoody(Goody):
     ''' A static goody - does not move from its initial position '''
 
@@ -15,16 +16,18 @@ class StaticGoody(Goody):
         ''' Stay where we are '''
         return STAY
 
+
 class RandomGoody(Goody):
     ''' A random-walking goody '''
 
     def take_turn(self, obstruction, _ping_response):
         ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
         possibilities = [PING]
-        for direction in [UP, DOWN, LEFT, RIGHT]: # STEP.keys()
+        for direction in [UP, DOWN, LEFT, RIGHT]:  # STEP.keys()
             if not obstruction[direction]:
                 possibilities.append(direction)
         return random.choice(possibilities)
+
 
 class GreedyGoody(Goody):
     ''' A goddy that pings once and then walks towards the other goody.  '''
@@ -46,26 +49,78 @@ class GreedyGoody(Goody):
 
         friend, = [player for player in self.last_ping_response.keys()
                    if isinstance(player, Goody) and player is not self]
-        foe, = [player for player in self.last_ping_response.keys() if isinstance(player, Baddy)]
+        foe, = [player for player in self.last_ping_response.keys()
+                                                                  if isinstance(player, Baddy)]
 
         # For the four possible moves, find the resulting distance to our friend after each one:
         last_known_friend_position = self.last_ping_response[friend]
         len_and_dirs = []
- 
-        for direction in [UP, DOWN, LEFT, RIGHT]: # STEP.keys()
+
+        for direction in [UP, DOWN, LEFT, RIGHT]:  # STEP.keys()
             # Choose the one that takes us closest to the our friend
             if not obstruction[direction]:
-                # STEP[direction] turns the direction label into a vector (dx, dy) which we can add 
+                # STEP[direction] turns the direction label into a vector (dx, dy) which we can add
                 # to a Position (another vector):
                 new_vector = last_known_friend_position - STEP[direction]
                 entry = [direction, new_vector, self.vector_len_2(new_vector)]
                 len_and_dirs.append(entry)
-        
+
         len_and_dirs.sort(key=lambda len_and_dir: len_and_dir[2])
         print(len_and_dirs)
-        
+
         return len_and_dirs[0][0]
 
 
-class YourGoody(Goody):
+class YourGoody1(Goody):
     ''' Your Goody implementation. Please change the name of this class to make it unique! '''
+    direction_list = [LEFT, DOWN, RIGHT, UP]
+    prev_i = 3
+    last_ping_response = None
+    n = 30
+    k = 30
+
+    def take_turn(self, obstruction, ping_response):
+        if self.n == 0:
+            if self.k == 0:
+                self.n = 30
+                self.k = 30
+            else:
+                possibilities = []
+                for direction in [UP, DOWN, LEFT, RIGHT]:  # STEP.keys()
+                    if not obstruction[direction]:
+                        possibilities.append(direction)
+                        return random.choice(possibilities)
+
+            
+        else:
+            self.n += -1
+            self.i = (self.prev_i + 1)%4
+            while obstruction[self.direction_list[self.i]]:
+                self.i = (self.i - 1)%4
+            self.prev_i = self.i
+            return self.direction_list[self.i]
+
+
+class YourGoody2(Goody):
+    direction_list = [LEFT, DOWN, RIGHT, UP]
+    prev_i = 2
+    n = 30
+    k = 30
+    def take_turn(self, obstruction, ping_response):
+        if self.n == 0:
+            if self.k == 0:
+                self.n = 30
+                self.k = 30
+            else:
+                self.k += -1
+                for direction in [UP, DOWN, LEFT, RIGHT]:  # STEP.keys()
+                    possibilities = []
+                    if not obstruction[direction]:
+                        possibilities.append(direction)
+                        return random.choice(possibilities)
+        
+        self.i = (self.prev_i - 1)%4
+        while obstruction[self.direction_list[self.i]]:
+            self.i = (self.i + 1)%4
+        self.prev_i = self.i
+        return self.direction_list[self.i]
